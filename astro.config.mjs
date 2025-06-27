d7a66064f1de03dda284e8da9d16660b2c48a852
Thiru N <thiru@spica.studio>
2025-06-27 19:43:45 +0530

    upgraded astro to v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static", // fully pre‑rendered
-  site: "https://example.com" // ← update when deploying
+  site: "https://thiru.me" // ← update when deploying
 });
